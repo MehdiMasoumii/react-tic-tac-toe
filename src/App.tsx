7833248c0679ce32d6f1854fb348b815c1612fd4
Mehdi Masoumi <dev.mahdimasoumi@gmail.com>
2024-07-27 18:08:22 +0330

    with time travel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,55 @@
-import GamePad from "./components/GamePad";
+import { useState } from "react";
+import GamePad, { Square } from "./components/GamePad";
 
 function App() {
+  const [history, setHistory] = useState([
+    Array(9).fill(null),
+  ]); /*array with single item which is an array of 9 nulls like what we did in GamePad State */
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove]; // because index starts from 0!
+  const turn: "X" | "O" = currentMove % 2 === 0 ? "X" : "O";
+
+  const handlePlay = (nextSquares: Square[]) => {
+    const tempHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(tempHistory);
+    setCurrentMove(tempHistory.length - 1); // because index starts from 0!
+  };
+
+  const handleReset = () => {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+  };
+
+  const jumpTo = (move: number) => {
+    setCurrentMove(move);
+  };
+
+  const moves = history.map((_squares, index) => {
+    const description = index > 0 ? `Go to move #${index}` : "Go to game start";
+    return (
+      <li key={index}>
+        <button
+          onClick={jumpTo.bind(null, index)}
+          className="bg-emerald-400 text-black px-2 py-1 rounded-sm"
+        >
+          {description}
+        </button>
+      </li>
+    );
+  });
+
   return (
-    <main className="flex items-center justify-center h-screen">
-      <GamePad />
+    <main className="flex items-center justify-center h-screen gap-8">
+      <div>
+        <GamePad
+          currentSquares={currentSquares}
+          turn={turn}
+          handlePlay={handlePlay}
+          handleReset={handleReset}
+        />
+      </div>
+      <ol className="flex flex-col gap-1">{moves}</ol>
     </main>
   );
 }
--- src/components/GamePad.tsx
+++ src/components/GamePad.tsx
@@ -1,37 +1,38 @@
-import { useState } from "react";
 import SquareButton from "./SquareButton";
-import useTurn from "../hooks/Turn";
 import CalculateWinner from "../assets/calculateWinner";
 
 export type Square = "X" | "O" | null;
 
-export default function GamePad() {
-  const [squares, setSquares] = useState<Square[]>(Array(9).fill(null));
-  const { turn, handleChangeTurn, setTurn } = useTurn("X");
-
-  const winner = CalculateWinner(squares);
+export default function GamePad({
+  currentSquares,
+  turn,
+  handlePlay,
+  handleReset,
+}: {
+  currentSquares: Square[];
+  turn: "X" | "O";
+  handlePlay: (nextSquares: Square[]) => void;
+  handleReset: () => void;
+}) {
+  const winner = CalculateWinner(currentSquares);
   const status = winner ? `Winner: ${winner}` : `Next player: ${turn}`;
 
   const handleClickChoice = (index: number) => {
-    if (squares[index] || winner) {
+    if (currentSquares[index] || winner) {
       return;
     }
-    const tempSquares = squares.slice();
-    tempSquares[index] = turn;
-    setSquares(tempSquares);
-    handleChangeTurn();
-  };
 
-  const handleReset = () => {
-    setSquares(Array(9).fill(null));
-    setTurn("X");
+    const tempSquares = currentSquares.slice();
+    tempSquares[index] = turn;
+
+    handlePlay(tempSquares);
   };
 
   return (
     <div className="flex flex-col items-center gap-4">
       <span className="text-xl font-semibold">{status}</span>
       <div className="grid grid-cols-3 grid-rows-3 w-fit border">
-        {squares.map((item, index) => (
+        {currentSquares.map((item, index) => (
           <SquareButton
             key={index}
             value={item}
@@ -41,10 +42,16 @@ export default function GamePad() {
       </div>
       <button
         onClick={handleReset}
-        className="bg-red-400 px-4 py-2 rounded-xl text-black text-lg font-medium hover:bg-red-500 transition-colors"
+        className="bg-red-400 px-6 py-1 rounded-sm text-black text-lg font-medium hover:bg-red-500 transition-colors"
       >
         reset
       </button>
+      <a
+        href="https://github.com/MehdiMasoumii"
+        className="text-blue-500 underline underline-offset-2"
+      >
+        by Mehdi Masoumi
+      </a>
     </div>
   );
 }
